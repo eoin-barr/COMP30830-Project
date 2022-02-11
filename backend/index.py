8a13a917c9adb87d37a29a05a79f0f27c19fd79b
harvey <harveygleeson@gmail.com>
2022-02-11 14:25:39 +0000

    Create be bike data scraper

--- backend/index.py
+++ backend/index.py
@@ -1,1 +1,27 @@
-# Index changess
+import requests
+import json 
+import time
+
+NAME="Dublin"
+STATIONS="https://api.jcdecaux.com/vls/v1/stations"
+KEY=""
+
+def main():
+
+    while True:
+        try:
+            # Request data from API
+            r = requests.get(STATIONS, params={"contract" : NAME, "apiKey" : KEY})
+            # Store the data
+            print(r.text)
+
+            # Wait for 5 mins
+            time.sleep(5*60)
+
+        except:
+            print(traceback.format_exc())
+    return
+
+if __name__ == "__main__":
+    main()
+
