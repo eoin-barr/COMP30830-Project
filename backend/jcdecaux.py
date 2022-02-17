4615ceb9c92d87ac5bac8f480e930d2b16b7f4e1
Eoin Barr <eoin.barr@ucdconnect.ie>
2022-02-17 17:15:03 +0000

    Add api logs

--- backend/jcdecaux.py
+++ backend/jcdecaux.py
@@ -3,6 +3,7 @@ import json
 import time
 import os
 from datetime import datetime
+import csv
 
 
 NAME="Dublin"
@@ -13,9 +14,12 @@ print(JCDECAUX_API_KEY)
 
 def main():
 
-    today = datetime.today()
-
     while True:
+        today = datetime.today()
+        current_date = today.strftime("%d/%m/%y")
+        current_time = today.strftime("%H:%M:%S")
+        data = ['JCDecaux', current_date, current_time]
+
         # try:
         try:
             # Request data from API
@@ -25,17 +29,25 @@ def main():
             parsed = json.loads(res.text)
             print(json.dumps(parsed, indent=4, sort_keys=True))
 
-            # Print current date and time
-            print(today.strftime("%d/%m/%y"))
-            print(today.strftime("%H:%M:%S"))
+            # Insert log into jcdecaux/complete.csv
+            with open('backend/logs/jcdecaux/complete.csv', 'a', newline='', encoding='UTF8') as f:
+                writer = csv.writer(f)
+                writer.writerow(data)
+                f.close()
 
         except:
             print("Error")
-            print(today.strftime("%d/%m/%y"))
-            print(today.strftime("%H:%M:%S"))
+            print(current_date)
+            print(current_time)
+
+            # Insert log into jcdecaux/error.csv
+            with open('backend/logs/jcdecaux/error.csv', 'a', newline='', encoding='UTF8') as f:
+                writer = csv.writer(f)
+                writer.writerow(data)
+                f.close()
         
         # Wait for 5 mins
-        time.sleep(5*60)
+        time.sleep(5)
 
         # except:
         #     print(traceback.format_exc())
