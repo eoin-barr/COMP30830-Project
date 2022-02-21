ef48115738b37de029edb6b002b2005c6ef2f1b9
Eoin Barr <eoin.barr@ucdconnect.ie>
2022-02-21 20:18:27 +0000

    Remove static command from jcd-dynamic request

--- api/jcd-dynamic.py
+++ api/jcd-dynamic.py
@@ -11,7 +11,6 @@ STATIONS="https://api.jcdecaux.com/vls/v1/stations"
 JCDECAUX_API_KEY = os.environ['JCDECAUX_API_KEY']
 print(JCDECAUX_API_KEY)
 
-
 def main():
 
     # Password needs to be inserted
@@ -22,7 +21,7 @@ def main():
         today = datetime.today()
         current_date = today.strftime("%d/%m/%y")
         current_time = today.strftime("%H:%M:%S")
-        data = ['JCDecaux', current_date, current_time]
+        data = ['JCDecaux(D)', current_date, current_time]
 
         # try:
         try:
@@ -31,7 +30,7 @@ def main():
             
             # Access most recent JSON response
             parsed = json.loads(res.text)
- 
+
             # For each station
             for station in parsed:
 
@@ -55,18 +54,14 @@ def main():
                 # Update last update to ymd format
                 last_update = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(last_update/1000))
                 
-                # Command statement for static
-                command_static = f"INSERT INTO static (address, name, lat, lng, number, banking, bonus) VALUES ('{address}', '{name}', '{lat}', '{lng}', '{number}', '{banking}', '{bonus}')"
-
                 # Command statement for dynamic
                 command_dynamic = f"INSERT INTO dynamic (address, available_bike_stands, available_bikes, bike_stands, last_update, status) VALUES ('{address}', '{available_bike_stands}', '{available_bikes}', '{bike_stands}', '{last_update}', '{status}')"
                 
                 # Execute the commands
-                #connection.execute(command_static)
                 connection.execute(command_dynamic)            
 
-            # Insert log into jcdecaux/complete.csv
-            with open('backend/logs/jcdecaux/complete.csv', 'a', newline='', encoding='UTF8') as f:
+            # Insert log into jcd-dynamic/complete.csv
+            with open('api/logs/jcd-dynamic/complete.csv', 'a', newline='', encoding='UTF8') as f:
                 writer = csv.writer(f)
                 writer.writerow(data)
                 f.close()
@@ -76,8 +71,8 @@ def main():
             print(current_date)
             print(current_time)
 
-            # Insert log into jcdecaux/error.csv
-            with open('backend/logs/jcdecaux/error.csv', 'a', newline='', encoding='UTF8') as f:
+            # Insert log into jcd-dynamic/error.csv
+            with open('api/logs/jcd-dynamic/error.csv', 'a', newline='', encoding='UTF8') as f:
                 writer = csv.writer(f)
                 writer.writerow(data)
                 f.close()
