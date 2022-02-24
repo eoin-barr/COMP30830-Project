1dfa090b0a116ab7187fa0c803b137c44c0c49b8
Eoin Barr <eoin.barr@ucdconnect.ie>
2022-02-24 18:46:11 +0000

    Add templates folder

--- web/app.py
+++ web/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def root():
-    return app.send_static_file('index.html')
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
