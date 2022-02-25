e43f099e883fef8ae7ad3da673f86d6d5f3bb3ec
Kevin O'Brien <kevin.o-brien.5@ucdconnect.ie>
2022-02-25 00:45:31 +0000

    fixed map error

--- web/static/map.js
+++ web/static/map.js
@@ -2,11 +2,11 @@ let map;
 
 async function initMap() {
     console.log('Helllloooooo')
-    map = await new google.maps.Map(document.getElementById("map"), {
+    map = await new google.maps.Map(document.getElementById('map'), {
       center: { lat: -34.397, lng: 150.644 },
       zoom: 8,
     });
   }
-
-initMap();
+  
+  initMap();
 
