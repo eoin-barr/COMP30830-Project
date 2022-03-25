2e4f512a0a7b1a7f679328d62768d4783b5411bf
Kevin O'Brien <kevin.o-brien.5@ucdconnect.ie>
2022-03-25 14:51:20 +0000

    Add dynamic data to onclick

--- web/app.py
+++ web/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, jsonify, g
 import os
 from sqlalchemy import create_engine
+import pandas as pd
+import json
+import datetime
 SQLPW = os.environ['SQLPW']
 #GMAPSKEY = os.environ['GMAPSKEY']
 
@@ -43,5 +46,37 @@ def maps():
     stations = get_stations()
     return render_template('maps.html', static_data=stations)
 
+@app.route("/occupancy/<station_name>")
+def get_occupancy(station_name):
+    engine = get_db()
+    dfrecentbike = pd.read_sql_query(f"SELECT available_bike_stands, max(last_update) FROM dynamic WHERE address='{station_name}'", engine)
+
+    #dfrecentbike['last_update_date'] = pd.to_datetime(dfrecentbike.last_update, unit='ms')
+    dfrecentbike.set_index('max(last_update)', inplace=True)
+    res = dfrecentbike['available_bike_stands']
+    #res['dt'] = df.index
+    print(res)
+    print("jsonify", type(jsonify(data=json.dumps(list(zip(map(lambda x: x.isoformat(), res.index), res.values))))))
+    dfrecentbike= dfrecentbike.to_json()
+    print("bike", type(dfrecentbike))
+    return dfrecentbike
+    #return jsonify(data=json.dumps(list(zip(map(lambda x: x.isoformat(), res.index), res.values))))
+
+
+    recent_bike = engine.execute(f"SELECT available_bike_stands, max(last_update) FROM dynamic WHERE address='{station_name}'")
+    json_data = {}
+
+    for result in recent_bike:
+        json_data['data']= result
+
+    json_data['data'][2] = json_data['data'][1].strftime("%H:%M:%S")
+    json_data['data'][1] = json_data['data'][1].strftime("%d/%m/%y")
+    print(json_data)
+    return json.dumps(json_data)
+    # for elem in recent_bike:
+    #     print(elem[0])
+    #     print(elem[1])
+    # return jsonify(data=json.dumps(recent_bike))
+    
 if __name__ == "__main__":
     app.run(debug=True)
