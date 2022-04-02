339a1354e37f7fbffa1bd30825cbdcfd3f1981af
Eoin Barr <eoin.barr@ucdconnect.ie>
2022-04-02 15:54:57 +0100

    Add directions api to dashboard

--- dashboard/client/src/components/map/map.tsx
+++ dashboard/client/src/components/map/map.tsx
@@ -1,26 +1,32 @@
-import React, { useEffect, useState } from "react";
 import {
-  GoogleMap,
-  useLoadScript,
   Marker,
+  GoogleMap,
   InfoWindow,
+  Autocomplete,
+  useJsApiLoader,
+  DirectionsRenderer,
 } from "@react-google-maps/api";
+import React, { useEffect, useState, useRef, useCallback } from "react";
 
 import { StationType } from ".";
 import { Sidebar } from "../sidebar";
 import { FillError } from "../error";
 import { FillLoading } from "../loading";
 import { getStations } from "../../lib/api";
-import { mapContainerStyle, center, options, libraries } from "../../lib/map";
+import { mapContainerStyle, center, options } from "../../lib/map";
 
 export function MapContainer() {
   const [stations, setStations] = useState<any>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [selected, setSelected] = useState<StationType | null>(null);
-  const { isLoaded, loadError } = useLoadScript({
+  const { isLoaded, loadError } = useJsApiLoader({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!,
-    libraries,
+    libraries: ["places"],
   });
+
+  const [distance, setDistance] = useState<String>("");
+  const [duration, setDuration] = useState<String>("");
+  const [directionsResponse, setDirectionsResponse] = useState<any>(null);
 
   useEffect(() => {
     const res = async () => {
@@ -31,12 +37,32 @@ export function MapContainer() {
     res();
   }, []);
 
-  const mapRef = React.useRef();
-  const onMapLoad = React.useCallback((map) => {
+  const originRef = useRef() as React.MutableRefObject<HTMLInputElement>;
+  const destinationRef = useRef() as React.MutableRefObject<HTMLInputElement>;
+
+  async function calculateRoute() {
+    if (originRef.current.value === "" || destinationRef.current.value === "") {
+      return;
+    }
+    const directionsService = new google.maps.DirectionsService();
+    const results = await directionsService.route({
+      origin: originRef.current.value,
+      destination: destinationRef.current.value,
+      travelMode: google.maps.TravelMode.DRIVING,
+    });
+    setDirectionsResponse(results);
+    //@ts-ignore
+    setDistance(results.routes[0].legs[0].distance.text);
+    //@ts-ignore
+    setDuration(results.routes[0].legs[0].duration.text);
+  }
+
+  const mapRef = useRef();
+  const onMapLoad = useCallback((map) => {
     mapRef.current = map;
   }, []);
 
-  const panTo = React.useCallback(({ lat, lng }) => {
+  const panTo = useCallback(({ lat, lng }) => {
     //@ts-ignore
     mapRef.current.panTo({ lat, lng });
     //@ts-ignore
@@ -49,7 +75,14 @@ export function MapContainer() {
 
   return (
     <div className='lg:flex lg:flex-row flex-col bg-black'>
-      <Sidebar stations={stations} panTo={panTo} />
+      <Sidebar
+        panTo={panTo}
+        stations={stations}
+        originRef={originRef}
+        Autocomplete={Autocomplete}
+        destinationRef={destinationRef}
+        calculateRoute={calculateRoute}
+      />
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
         zoom={14}
@@ -57,6 +90,9 @@ export function MapContainer() {
         options={options}
         onLoad={onMapLoad}
       >
+        {directionsResponse && (
+          <DirectionsRenderer directions={directionsResponse} />
+        )}
         {stations &&
           stations.map((station: StationType) => (
             <Marker
--- dashboard/client/src/components/sidebar/sidebar.tsx
+++ dashboard/client/src/components/sidebar/sidebar.tsx
@@ -5,12 +5,24 @@ import { StationType } from "../map";
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
   panTo: any;
+  originRef: any;
+  Autocomplete: any;
+  destinationRef: any;
+  calculateRoute: any;
   stations: StationType[];
 }
 
 export function Sidebar(props: Props) {
-  const { stations, panTo, ...rest } = props;
-  const classes = `lg:flex-col justify-start items-start absolute top-0 z-10 lg:h-full h-101 bg-black lg:min-w-56 lg:w-56 w-full min-h-101 px-4 py-4`;
+  const {
+    panTo,
+    stations,
+    originRef,
+    Autocomplete,
+    destinationRef,
+    calculateRoute,
+    ...rest
+  } = props;
+  const classes = `lg:flex-col justify-start items-start absolute top-0 z-10 lg:h-full h-101 bg-primary-black lg:min-w-56 lg:w-56 w-full min-h-101 px-4 py-4`;
   return (
     <div className={classes} {...rest}>
       <div>
@@ -19,10 +31,38 @@ export function Sidebar(props: Props) {
         </h1>
         {stations && <CustomInput panTo={panTo} stations={stations} />}
       </div>
-      <div className='pt-8'>
+      <div className='flex-col pt-8'>
         <h1 className='font-[400] text-primary-grey2 text-2xl'>
           Plan Your Journey
         </h1>
+        <div className='pt-2'>
+          <Autocomplete>
+            <input
+              className='py-2 px-4 rounded text-sm'
+              type='text'
+              placeholder='Origin'
+              ref={originRef}
+            />
+          </Autocomplete>
+        </div>
+        <div className='pt-2'>
+          <Autocomplete>
+            <input
+              className='py-2 px-4 rounded text-sm'
+              type='text'
+              placeholder='Destination'
+              ref={destinationRef}
+            />
+          </Autocomplete>
+        </div>
+        <div className='pt-2'>
+          <button
+            className='py-2 px-4 bg-blue-500 text-white rounded-md'
+            onClick={calculateRoute}
+          >
+            Calculate Route
+          </button>
+        </div>
       </div>
     </div>
   );
