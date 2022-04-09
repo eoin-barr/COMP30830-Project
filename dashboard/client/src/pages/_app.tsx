39e961c885ac9cc3b0c9e071bc7f55063848b476
Eoin Barr <eoin.barr@ucdconnect.ie>
2022-04-09 13:42:23 +0100

    Add favicons

--- dashboard/client/src/pages/_app.tsx
+++ dashboard/client/src/pages/_app.tsx
@@ -1,10 +1,24 @@
+import Head from "next/head";
 import type { AppProps } from "next/app";
 
 import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link
+          rel='shortcut icon'
+          type='image/png'
+          href='https://res.cloudinary.com/dk0r9bcxy/image/upload/v1638291047/WAD/logo4_bpwnsi.png'
+        />
+        <title>Bikes Locator</title>
+        <meta name='description' content='Find bikes near you' />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
