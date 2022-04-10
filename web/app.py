1c4cf5d0d8a24187728d5055ce76c9fa246d9340
Eoin Barr <eoin.barr@ucdconnect.ie>
2022-04-10 18:13:45 +0100

    Update calendar select css

--- web/app.py
+++ web/app.py
@@ -62,8 +62,8 @@ def get_db():
     return db
 
 def get_hour_means():
-    #jsonfile = open('./../web/hour_means_json.json', "r")
-    jsonfile = open('web/hour_means_json.json', "r")
+    jsonfile = open('./../web/hour_means_json.json', "r")
+    # jsonfile = open('web/hour_means_json.json', "r")
     hour_data = json.load(jsonfile)
     res = hour_data
     jsonfile.close()
@@ -71,16 +71,16 @@ def get_hour_means():
 
 @app.route("/get-hour-means")
 def get_hour_means_route():
-    #jsonfile = open('./../web/hour_means_json.json', "r")
-    jsonfile = open('web/hour_means_json.json', "r")
+    jsonfile = open('./../web/hour_means_json.json', "r")
+    # jsonfile = open('web/hour_means_json.json', "r")
     hour_data = json.load(jsonfile)
     res = hour_data
     jsonfile.close()
     return res
 
 def get_day_means():
-    #jsonfile = open('./../web/day_means_json.json', "r")
-    jsonfile = open('web/day_means_json.json', "r")
+    jsonfile = open('./../web/day_means_json.json', "r")
+    # jsonfile = open('web/day_means_json.json', "r")
     day_data = json.load(jsonfile)
     res = day_data
     jsonfile.close()
@@ -88,8 +88,8 @@ def get_day_means():
 
 @app.route("/get-day-means")
 def get_day_means_route():
-    #jsonfile = open('./../web/day_means_json.json', "r")
-    jsonfile = open('web/day_means_json.json', "r")
+    jsonfile = open('./../web/day_means_json.json', "r")
+    # jsonfile = open('web/day_means_json.json', "r")
     day_data = json.load(jsonfile)
     res = day_data
     jsonfile.close()
